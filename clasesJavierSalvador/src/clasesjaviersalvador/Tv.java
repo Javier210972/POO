4ba968790e0d182b54ae15802d9c87b40e87bb00
Javier210972 <37030402+Javier210972@users.noreply.github.com>
2018-03-12 11:37:14 -0600

    Update Tv.java

--- clasesJavierSalvador/src/clasesjaviersalvador/Tv.java
+++ clasesJavierSalvador/src/clasesjaviersalvador/Tv.java
@@ -10,43 +10,57 @@ package clasesjaviersalvador;
  * @author toshiba2
  */
 public class Tv {
-    private int nocanal;
+
+    private int nocanal=1;
     private int volumencanal;
-   private boolean estado;
-   
+    private boolean estado;
+
+    public Tv() {
+    }
+
+    public Tv(int nocanal, int volumencanal, boolean estado) {
+        this.nocanal = nocanal;
+        this.volumencanal = volumencanal;
+        this.estado = false;
+
+    }
+
+    public boolean encendido() {
+        if (estado == false) {
+            this.estado = true;
+            System.out.println("Tv encendida, se encuentra en el canal 1");
+        } else {
+            this.estado = false;
+            System.out.println("Tv apagada");
+
+        }
+        return this.estado;
+    }
+public boolean encendido2() {
+        this.estado=true;
+        return this.estado;
+    }
     
-   
-public Tv() {
- }
-
-public Tv(int nocanal,int volumencanal,boolean estado){
- this.nocanal=nocanal;
- this.volumencanal=volumencanal;
- this.estado=false;
-
-}
-public void encendido(){
-    if(estado==false){
-    this.estado=true;
-        System.out.println("Tv encendida, se encuentra en el canal 1");
-    }
-    else {
-        this.estado=false;
-        System.out.println("Tv apagada");
-        
-    
-    }
-}
-
-    public int getNocanal() {
-       
-        return nocanal=2;
-    }
-
     public void setNocanal(int nocanal) {
-        
+        this.nocanal = nocanal;
 
+    }
+
+    public int getnocanalS() {
+      this.nocanal=this.nocanal+1;
+        if (this.nocanal==5) {
             
+           this.nocanal=1; 
+        }
+        return nocanal;
+    }
+    public int getnocanalR() {
+      this.nocanal=this.nocanal-1;
+        if (this.nocanal==1) {
+            
+           this.nocanal=5; 
+        }
+        return nocanal;
     }
 
     public int getVolumencanal() {
@@ -64,10 +78,5 @@ public void encendido(){
     public void setEstado(boolean estado) {
         this.estado = estado;
     }
-
-
-
-
-
 
 }
