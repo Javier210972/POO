2dc48602bf1ac9453eec4498a404b339063aa398
Javier210972 <37030402+Javier210972@users.noreply.github.com>
2018-03-14 10:50:54 -0600

    Update Tv.java

--- clasesJavierSalvador/src/clasesjaviersalvador/Tv.java
+++ clasesJavierSalvador/src/clasesjaviersalvador/Tv.java
@@ -11,9 +11,9 @@ package clasesjaviersalvador;
  */
 public class Tv {
 
-    private int nocanal=1;
-    private int volumencanal;
-    private boolean estado;
+    private int nocanal = 1;
+    private int volumencanal=10;
+    private boolean estado = false;
 
     public Tv() {
     }
@@ -28,42 +28,108 @@ public class Tv {
     public boolean encendido() {
         if (estado == false) {
             this.estado = true;
-            System.out.println("Tv encendida, se encuentra en el canal 1");
+            
+            System.out.println("TV Encendida, Canal:1");
         } else {
             this.estado = false;
-            System.out.println("Tv apagada");
+            System.out.println("TV Apadado");
 
         }
         return this.estado;
     }
-public boolean encendido2() {
-        this.estado=true;
+
+    public boolean encendido2() {
+        this.estado = true;
         return this.estado;
     }
-    
+
     public void setNocanal(int nocanal) {
         this.nocanal = nocanal;
 
     }
 
     public int getnocanalS() {
-      this.nocanal=this.nocanal+1;
-        if (this.nocanal==5) {
-            
-           this.nocanal=1; 
-        }
-        return nocanal;
-    }
-    public int getnocanalR() {
-      this.nocanal=this.nocanal-1;
-        if (this.nocanal==1) {
-            
-           this.nocanal=5; 
+
+        if (this.estado == true) {
+
+            this.nocanal = this.nocanal + 1;
+            System.out.println("Subio al canal:" + this.nocanal);
+            if (this.nocanal == 5) {
+
+                this.nocanal = 0;
+            }
+
+        } else if (this.estado == false) {
+            System.out.println("TV Apagada, enciendala");
         }
         return nocanal;
     }
 
-    public int getVolumencanal() {
+    public int getnocanalR() {
+
+        if (this.estado == true) {
+            if (this.nocanal == 0) {
+
+                this.nocanal = 5;
+            }
+            if (this.nocanal == 1) {
+
+                this.nocanal = 6;
+            }
+
+            this.nocanal = this.nocanal - 1;
+            System.out.println("Bajo al canal:" + this.nocanal);
+        } 
+        else if (this.estado == false) {
+            System.out.println("TV Apagada, enciendala");
+        }
+        return nocanal;
+    }
+
+    public int getVolumencanalS() {
+        if (estado == true) {
+             if (this.volumencanal==25) 
+            {
+              this.volumencanal=25;
+            }
+             else
+             {
+                 this.volumencanal++;
+             }
+               
+            
+            
+             System.out.println("Subio el volumen a:" + this.volumencanal);
+             
+                
+        }
+       
+         else if (this.estado == false) {
+            System.out.println("TV Apagada, enciendala");
+        }
+        return volumencanal;
+    }
+    public int getVolumencanalB() {
+        if (estado == true) {
+             if (this.volumencanal==1) 
+            {
+              this.volumencanal=1;
+            }
+             else
+             {
+                 this.volumencanal--;
+             }
+               
+            
+            
+             System.out.println("Bajo el volumen a:" + this.volumencanal);
+             
+                
+        }
+       
+         else if (this.estado == false) {
+            System.out.println("TV Apagada, enciendala");
+        }
         return volumencanal;
     }
 
